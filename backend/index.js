2de7afa4db259869fe4da2770284ae2dfc908e3d
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-05-14 18:24:54 +0530

    cors chang

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,14 @@ const questionRoutes = require('./src/routes/question.routes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: ['https://track-dsa.vercel.app', 'http://localhost:5173'], // Allow both your deployed frontend and local frontend
+  methods: 'GET,POST,PUT,DELETE',           // Allowed methods
+  allowedHeaders: 'Content-Type,Authorization',  // Allowed headers
+};
+
+app.use(cors(corsOptions));
+
 // Ensure MONGO_URI is available
 if (!process.env.MONGO_URI) {
   console.error('Mongo URI is not defined in .env file');
@@ -21,7 +29,7 @@ if (!process.env.MONGO_URI) {
 connectDB();
 
 // Middleware
-app.use(cors());
+
 app.use(express.json());
 
 // Routes
