7ceccb7e78c0e82578de5a4c1ca5831cb063ed98
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-05-20 10:49:31 +0530

    set token to localstorage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Router from './routes/router'; // Import the Router component
 
 const App = () => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(() => localStorage.getItem('token') || ''); // Initialize token from localStorage
 
   return (
     <div>
